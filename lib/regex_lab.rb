113031273de9cf3bdf782867c603b75d5c86b236
Michael Boroski <mjboroski@gmail.com>
2018-04-19 02:50:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,17 +3,17 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b\w^un+$ing\b/)
 end
 
 def words_five_letters_long(text)
-text.scan(a{5}\w)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(//)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(//)
 end
