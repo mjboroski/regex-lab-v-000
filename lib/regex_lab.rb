b88a6cf0361028f65d6ed39014b6479fe0526000
Michael Boroski <mjboroski@gmail.com>
2018-04-19 02:46:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-text.match(a{5})
+text.scan(a{5}\w)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
